8fa572234bd5f52e64c59ecaa5777741b9e99cd1
Mark Wagner <mwagner72@gmail.com>
2013-08-17 20:11:09 -0500

    Get students, teachers, and classes through schools

--- lib/clever-ruby/school.rb
+++ lib/clever-ruby/school.rb
@@ -6,4 +6,27 @@ module Clever
       [:state_id, :sis_id, :nces_id, :low_grade, :high_grade, :principal, :location, :phone]
     end
   end
+
+  def teachers(filters={})
+    get_linked_resources 'teachers', filters
+  end
+
+  def sections(filters={})
+    get_linked_resources 'sections', filters
+  end
+
+
+  def students(filters={})
+    get_linked_resources 'students', filters
+  end
+
+  private
+
+  def get_linked_resources(resource_type, filters={})
+    refresh
+    uri = links.detect { |link| link[:rel] == resource_type }[:uri]
+    response = Clever.request(:get, uri, filters)
+    Util.convert_to_clever_object(response[:data])
+  end
+
 end
