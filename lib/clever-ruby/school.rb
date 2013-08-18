a587242947fd617493013a6dbe63a37547354b6d
Mark Wagner <mwagner72@gmail.com>
2013-08-18 12:49:37 -0500

    get all resources through linked

--- lib/clever-ruby/school.rb
+++ lib/clever-ruby/school.rb
@@ -24,9 +24,27 @@ module Clever
 
     def get_linked_resources(resource_type, filters={})
       refresh
+
+      page_count     = 1000
+      page_n         = 1
+      filters[:limit] = page_count
+      filters[:page] = page_n
+      objects        = []
       uri = links.detect { |link| link[:rel] == resource_type }[:uri]
       response = Clever.request(:get, uri, filters)
-      Util.convert_to_clever_object(response[:data])
+      page = Util.convert_to_clever_object(response[:data])
+      objects += page
+
+      while page.size == page_count
+        page_n  += 1
+        filters[:page] = page_n
+        response = Clever.request(:get, uri, filters)
+        page = Util.convert_to_clever_object(response[:data])
+        objects += page
+        puts "Fetched #{page.size}/#{objects.size} (page #{page_n}) of #{self.name.gsub("Clever::", "")}" if ENV["DEBUG"]
+      end
+
+      objects.compact
     end
   end
 end
--- lib/clever-ruby/version.rb
+++ lib/clever-ruby/version.rb
@@ -1,3 +1,3 @@
 module Clever
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
